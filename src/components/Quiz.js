75c351eead2ea792d36a35cab7509ee1d45e51ef
Sachin <sachin.yadav607@gmail.com>
2022-06-04 22:04:01 +0530

    Check answer added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React from "react";
 import parse from "html-react-parser";
-import { nanoid } from 'nanoid';
-import './Quiz.css'
-export default function Quiz(props){
-   const {incorrect_answers,question} = props;
-   const answerOptions = incorrect_answers.map(ans=>(<button key={nanoid()} className='answer--button'>{parse(ans)}</button>))
+import { nanoid } from "nanoid";
+import "./Quiz.css";
+export default function Quiz(props) {
+  const {
+    incorrect_answers,
+    correct_answer,
+    question,
+    selection,
+    result,
+    handleClick,
+    id,
+  } = props;
+  const allOptions = [...incorrect_answers, correct_answer];
 
+  /* function shuffleArray(arr) {
+    const newArr = [];
+    while (newArr.length < 4) {
+      const num = Math.floor(Math.random() * 4);
+      if (!newArr.indexOf(num)) newArr.push(num);
+    }
+    return newArr
+  } */
+  const answerOptions = allOptions.map((ans) => (
+    <button
+      key={nanoid()}
+      className={`answer--button ${
+        result
+          ? selection === ans
+            ? selection === correct_answer
+              ? "correct--answer"
+              : "wrong--answer"
+            : ""
+          : selection === ans
+          ? "answer--selected"
+          : ""
+      }`}
+      onClick={() => handleClick(ans, id)}
+    >
+      {parse(ans)}
+    </button>
+  ));
 
-    return(
-        <div className='question--box'>
-             <h3>{parse(question)}</h3>
-             <div className='answer--option'>{answerOptions}</div>
-        </div>
-
-    )
+  return (
+    <div className="question--box">
+      <h3>{parse(question)}</h3>
+      <div className="answer--option">{answerOptions}</div>
+    </div>
+  );
 }
--- src/components/Quizpage.js
+++ src/components/Quizpage.js
@@ -1,19 +1,69 @@
 import React from "react";
-import Quiz from './Quiz'
-import { nanoid } from 'nanoid';
+import Quiz from "./Quiz";
+import { nanoid } from "nanoid";
 import "./Quizpage.css";
 
-export default function Quizpage(){
-    const [quizData, setQuizData] = React.useState([])
+export default function Quizpage() {
+  const [quizData, setQuizData] = React.useState([]);
+  const [result, setResult] = React.useState(false);
+  const [correctAnswer, setCorrectAnswer] = React.useState(0);
 
-    React.useEffect(()=>{ console.log("effect")
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then(res=>res.json())
-        .then(quiz=>setQuizData(quiz.results))
-    },[])
-    const elements = quizData.map(quiz=> <Quiz key={nanoid()} {...quiz}/>)
+function fetchQuiz(){
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    .then((res) => res.json())
+    .then((quiz) =>
+      setQuizData(
+        quiz.results.map((item) => ({ ...item, id: nanoid(), selection: "" }))
+      )
+    );
+}
+  React.useEffect(() => {
+    console.log("effect");
+    fetchQuiz()
+  }, []);
+  const elements = quizData.map((quiz) => (
+    <Quiz key={quiz.id} {...quiz} handleClick={handleClick} result={result} />
+  ));
+  function handleClick(val, id) {
+    setQuizData(
+      quizData.map((item) =>
+        item.id === id
+          ? { ...item, selection: item.selection === val ? "" : val }
+          : item
+      )
+    );
+  }
+  function checkAnswer() {
+    if (!result) {
+      let correctAnswer = 0;
+      quizData.map((quiz) => {
+        if (quiz.selection === quiz.correct_answer) correctAnswer++;
+        return true;
+      });
+      setResult(true);
+      setCorrectAnswer(correctAnswer);
+    }
+    else{
+        setResult(false);
+        setCorrectAnswer(0);
+        fetchQuiz();
+    }
+  }
+  //console.log(">>>" + JSON.stringify(quizData));
+  return (
+    <div style={{ zIndex: 1, textAlign: "left" }}>
+      {elements}
+      <div className="check--answer">
+        {result && (
+          <span className="score">
+            You scored {correctAnswer}/5 correct answers
+          </span>
+        )}
 
-    return(
-        <div style={{zIndex:1,textAlign:"left"}}>{elements}</div>
-    )
+        <button onClick={checkAnswer}>
+          {!result ? "Check answer" : "Play again"}
+        </button>
+      </div>
+    </div>
+  );
 }
